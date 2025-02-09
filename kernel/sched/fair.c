399d9d4f46981f3f58cd47ab9c2e5a6643374c3d
deflamingq o <sukairo71@gmail.com>
2025-02-09 05:39:34 +0000

    remove #ifdef

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5841,7 +5841,6 @@ static inline void hrtick_update(struct rq *rq)
 }
 #endif
 
-#ifdef CONFIG_SMP
 bool cpu_overutilized(int cpu)
 {
 	unsigned long rq_util_min = uclamp_rq_get(cpu_rq(cpu), UCLAMP_MIN);
