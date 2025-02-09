a8a9900486bbe24bffeeda63b182c6f846796a15
deflamingq o <sukairo71@gmail.com>
2025-02-09 00:42:02 +0000

    tune: fix redefinition

--- kernel/sched/tune.h
+++ kernel/sched/tune.h
@@ -30,7 +30,6 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 #else
 #define schedtune_task_boost(tsk) 0
 #endif
-#define schedtune_task_boost_rcu_locked(tsk) 0
 
 #ifdef CONFIG_UCLAMP_TASK_GROUP
 #define schedtune_prefer_idle(tsk) uclamp_latency_sensitive(tsk)
@@ -43,7 +42,6 @@ void schedtune_dequeue_task(struct task_struct *p, int cpu);
 #else
 #define schedtune_prefer_high_cap(tsk) 0
 #endif
-#define schedtune_prefer_high_cap(tsk) 0
 
 #define schedtune_enqueue_task(task, cpu) do { } while (0)
 #define schedtune_dequeue_task(task, cpu) do { } while (0)
