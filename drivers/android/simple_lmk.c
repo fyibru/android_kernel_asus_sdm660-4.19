1e8a38025e0d9c02cea42109ee383264cf34eb0f
deflamingq o <sukairo71@gmail.com>
2025-02-12 14:02:12 +0000

    simple_lmk: wait 3seconds to starting killing procces again

--- drivers/android/simple_lmk.c
+++ drivers/android/simple_lmk.c
@@ -260,7 +260,7 @@ static void scan_and_kill(void)
 	if (!wait_for_completion_timeout(&reclaim_done, RECLAIM_EXPIRES))
 		pr_info("Timeout hit waiting for victims to die, proceeding\n");
 	else
-		msleep(28);
+		msleep(3000);
 
 	/* Clean up for future reclaim invocations */
 	write_lock(&mm_free_lock);
